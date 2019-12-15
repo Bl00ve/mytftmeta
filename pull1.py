b620d0b826207025d0cabe545d2382795cfc499e
Thomas <tcblauvelt@gmail.com>
2019-12-15 06:19:38 -0800

    editing classes to take multiple matches

--- pull1.py
+++ pull1.py
@@ -4,11 +4,11 @@ import requests, time
 REGION = 'na1'
 TFTREGION = 'americas'
 APIKEY = 'RGAPI-9e42a420-48a8-4658-a50b-569bd106691c'
-
+MATCHES = '2'
 # Class 
 class Summoner:
     def __init__(self, name):
-        if len(name) < 40:
+        if len(name) < 17:
             URL = 'https://' + REGION + '.api.riotgames.com/tft/summoner/v1/summoners/by-name/' + name + '?api_key=' + APIKEY
         else:
             URL = 'https://' + REGION + '.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/' + name + '?api_key=' + APIKEY
@@ -17,18 +17,17 @@ class Summoner:
         self.puuid = response.json()['puuid']
         self.accountid = response.json()['accountId']
         self.name = response.json()['name']
+        self.history = requestTFTMatchHistory(self.puuid, TFTREGION, APIKEY, MATCHES)
 
-class Match:
-    def __init__(self, summoner):
-        self.summoner = summoner
-        ID = self.summoner.puuid
-        self.history = requestTFTMatchHistory(ID, TFTREGION, APIKEY, Match.matches)
-        self.details = requestMatchDetails(self.history, TFTREGION, APIKEY)
-        self.game_datetime = self.details['info']['game_datetime']
-        self.game_version = self.details['info']['game_version']
-        self.match_id = self.details['metadata']['match_id']
-        self.match_participants = self.details['metadata']['participants']
-
+class MatchDetails:
+    def __init__(self, match_ids):
+        self.info = []
+        for match_id in match_ids:
+            self.info.append(requestMatchDetails(match_id, TFTREGION, APIKEY))
+        self.match_participants = []
+        for match in self.info:
+            for participant in match['metadata']['participants']:
+                self.match_participants.append(Summoner(participant).name)
 
 def requestSummonerData(REGION, summonerName, APIKEY):
     URL = 'https://' + REGION + '.api.riotgames.com/tft/summoner/v1/summoners/by-name/' + summonerName + '?api_key=' + APIKEY
@@ -43,32 +42,28 @@ def requestTFTSummonerData(ID, REGION, APIKEY, playerlist, uniqueplayerlist):
     response = requests.get(URL)
     return response.json()
 def requestMatchDetails(matchID, TFTREGION, APIKEY):
-    for match in matchID:
-        URL = 'https://' + TFTREGION + '.api.riotgames.com/tft/match/v1/matches/' + match + '?api_key=' + APIKEY
-        response = requests.get(URL)
-        return response.json()
+    URL = 'https://' + TFTREGION + '.api.riotgames.com/tft/match/v1/matches/' + matchID + '?api_key=' + APIKEY
+    response = requests.get(URL)
+    return response.json()
 
 
 def main():
     #Set static values for just me
-    Match.matches = '2'
     a_summoner = Summoner('Bloodvault')
 
 
-    Match(a_summoner)
-    print(a_summoner.name)
-    print(Match(a_summoner).history)
+    #Match(a_summoner)
+    #print(a_summoner.name)
+    #print(Match(a_summoner).history)
+
     #print(Match(a_summoner).history[1])
     #print(Match(a_summoner).game_datetime)
     
     #This will be for grabbing people's units
     #print(Match(a_summoner).details['info']['participants'])
 
-    print(Match(a_summoner).match_participants)
+    print(MatchDetails(Summoner(a_summoner.name).history).match_participants)
     #print(Match(a_summoner).details['metadata'])
 
-    for person in Match(a_summoner).match_participants:
-        b_summoner = Summoner(person)
-        print(b_summoner.name)
 if __name__ == '__main__':
     main()
